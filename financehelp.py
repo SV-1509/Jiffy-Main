fb95e3f1762aa06dbe2d90470804dc6e48619a97
Sreevishakh V <43301384+SV-1509@users.noreply.github.com>
2020-12-25 13:59:36 +0530

    Update financehelp.py

--- financehelp.py
+++ financehelp.py
@@ -1,13 +1,16 @@
-
+import gensim
 import make as m
 import csv
-finder = m.makedict()
-# import csv
-# reader = csv.reader(open('filename.csv', 'r'))
-# finder = {}
-# for row in reader:
-#    k, v = row
-#    finder[k] = v
+
+
+import re
+model = gensim.models.KeyedVectors.load_word2vec_format('glove.txt', binary = False)
+
+reader = csv.reader(open('dict.csv', 'r'))
+finder = {}
+for row in reader:
+   k, v = row
+   finder[k] = v
 			
 		
 def findfunction(x):
@@ -18,11 +21,20 @@ def findfunction(x):
 			functions = functions + i + " "
 		print("You might be looking for {}".format(functions) )
 	except:
-		print("No functions found ! Try a different keyword")
+		try:
+			a = model.most_similar(positive = [x])[:5]
+			print(a)
+			for j in a :
+				functions = ""
+				for i in finder[j[0]]:
+					functions = functions + i + " "
+			print("You might be looking for {}".format(functions) )
+		except:
+			print("No functions found ! Try a different keyword")
 
-def helpfunction(x):
-	x=x.lower()
-	try :
-		print(helper[x])
-	except:
-		print("No function found")
+# def helpfunction(x):
+# 	x=x.lower()
+# 	try :
+# 		print(helper[x])
+# 	except:
+# 		print("No function found")
