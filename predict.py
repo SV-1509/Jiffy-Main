0fa7b5ce1ef9ca4204cfe70fa906cc217edc7125
Sanchit Sreekanth <62935958+sanchit08@users.noreply.github.com>
2021-03-12 11:06:04 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,11 @@
 import gensim
+import os
 
-
-model = gensim.models.KeyedVectors.load_word2vec_format('word2vec.txt', binary = False)
-print(model.similarity('profit','gain'))
-print(model.most_similar(positive = ['company'],negative = ['expenditure']))
-
-
+#function to check sample inputs to the model
+path = os.getcwd() + '/models/'
+#change the model name in the line below to 'word2vec.txt' to load the Word2Vec model
+model = gensim.models.KeyedVectors.load_word2vec_format(path + 'glove.txt', binary = False)
+ans = model.similarity('profit','gain')
+print(ans)
+a = model.most_similar(positive = ['company'],negative = ['expenditure'])[:2]
+print(a)
