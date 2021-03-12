129ebf89cba448e9628a0148c2171bd3656fba2d
Sanchit Sreekanth <62935958+sanchit08@users.noreply.github.com>
2021-03-12 10:54:18 +0530

    Update clean.py

--- clean.py
+++ clean.py
@@ -6,10 +6,13 @@ from nltk.corpus import stopwords, wordnet
 import nltk
 
 
-
+#remove stopwords and lemmatize sentence
 stopwords = stopwords.words('english')
 lemmatizer = WordNetLemmatizer()
 
+#FUNCTIONS
+
+#nltk tagged words
 def nltk_tag_to_wordnet_tag(nltk_tag):
 	if nltk_tag.startswith('J'):
 		return wordnet.ADJ
@@ -22,6 +25,7 @@ def nltk_tag_to_wordnet_tag(nltk_tag):
 	else:          
 		return None
 
+#lemmatization
 def lemmatize_sentence(sentence):
 	nltk_tagged = nltk.pos_tag(sentence)
 	wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
@@ -33,7 +37,7 @@ def lemmatize_sentence(sentence):
 			lemmatized_sentence.append(lemmatizer.lemmatize(word,tag))
 	return lemmatized_sentence
 
-
+#clean the text by removing punctuations, numbers, stopwords and lemmatize sentences
 def clean(doc):
 	doc = ''.join([i for i in doc if not i.isdigit()])
 	doc = doc.lower()
@@ -43,4 +47,3 @@ def clean(doc):
 	filtered_words = [word for word in word_list if word not in stopwords and len(word)>2]
 	lem_text = lemmatize_sentence(filtered_words)
 	return lem_text
-#print(clean('Bootstrap.caplet volatilities from cap volatilities using similar notation to Hull’s book (page 32.11). The first volatility in the vector of caplet vols is zero.'))
