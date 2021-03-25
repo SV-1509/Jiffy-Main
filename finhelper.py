386543069a854a7d09720bcea7afff15aa064c29
Sanchit Sreekanth <62935958+sanchit08@users.noreply.github.com>
2021-03-25 12:37:30 +0530

    Update finhelper.py

--- finhelper.py
+++ finhelper.py
@@ -8,16 +8,14 @@ import gensim
 import os
 
 
-#function to print the results in the required format
-def print_format(dictionary,word):
+def print_format(l):
 	print('here are the results \n')
-	dictionary[word] = [x for x in dictionary[word] if x != '']
-	for i in range(len(dictionary[word])):
-		print(str(i) + ' - '+dictionary[word][i])
+	for i in range(len(l)):
+		print(str(i) + ' - '+l[i])
 	a = input('\nwant to get more details?(y/n) ')
 	if a.lower() == 'y':
-		if len(dictionary[word]) == 1:
-			index = functions.index(dictionary[word][0])
+		if len(l) == 1:
+			index = functions.index(l[0])
 			print('\n')
 			print("SYNTAX: ")
 			print(syntax[index])
@@ -26,7 +24,7 @@ def print_format(dictionary,word):
 			print(description[index])
 		else:
 			n = int(input('\nenter the function:'))
-			index = functions.index(dictionary[word][n])
+			index = functions.index(l[n])
 			print('\n')
 			print("SYNTAX: ")
 			print(syntax[index])
@@ -34,14 +32,12 @@ def print_format(dictionary,word):
 			print('DESCRIPTION: ')
 			print(description[index])
 
-
 lemmatizer = WordNetLemmatizer()
 cwd = os.getcwd()
 #get user input word as a string
 word = input('enter word: ')
 word = lemmatize_sentence([word])[0]
 
-#load the primary helper dictionary
 reader = csv.reader(open(cwd + '/csv/helper_dictionary_3.csv', 'r'))
 dictionary = {}
 for row in reader:
@@ -49,49 +45,42 @@ for row in reader:
 	value = row[1:]
 	dictionary[key] = value
 
-#load the financepy function names, syntax and description
 df = pd.read_csv(cwd + '/csv/financepy.csv')
 functions = df['func'].tolist()
 syntax = df['arg'].tolist()
 description = df['desc'].tolist()
 
+reader2 = csv.reader(open(cwd + '/csv/description.csv', 'r'))
+dictionary2 = {}
+for row in reader2:
+	key = row[0]
+	value = row[1:]
+	dictionary2[key] = value
 
-#check if word is in dictionary
-if word in dictionary:
-	print_format(dictionary,word)
-#check for function name matches with input string
-else :
+if word not in dictionary and word not in dictionary2:
 	func = []
 	for i in functions:
 		func.append(i.split('.')[-1].lower())
 	suggestions = []
 	for i in func:
 		if word in i:
-			suggestions.append(i)
+			suggestions.append(functions[i])
 	if len(suggestions) > 0:
-		print('here are the results \n')
-		for i in range(len(suggestions)):
-			index = func.index(suggestions[i])
-			print(str(i) + ' - '+functions[index])
-		a = input('\nwant to get more details?(y/n) ')
-		if a.lower() == 'y':
-			if len(suggestions) == 1:
-				index = func.index(suggestions[0])
-				print('\n')
-				print("SYNTAX: ")
-				print(syntax[index])
-				print('\n')
-				print('DESCRIPTION: ')
-				print(description[index])
-			else:
-				n = int(input('\nenter the function:'))
-				index = func.index(suggestions[n])
-				print('\n')
-				print("SYNTAX: ")
-				print(syntax[index])
-				print('\n')
-				print('DESCRIPTION: ')
-				print(description[index])
+		print_format(suggestions)
+
+
 	else:
 		print("not found")
-	
+else:
+	print('yes')
+
+	if word in dictionary:
+		v1 = dictionary[word]
+	else:
+		v1 = []
+	if word in dictionary2:
+		v2 = dictionary2[word]
+	else:
+		v2 = []
+	v = list(set(v1+v2))
+	print_format(v)
